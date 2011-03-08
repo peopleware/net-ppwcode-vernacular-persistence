20e6849f230443cccdfc6444c3db116d3125d1f8
Jan Dockx <jan_dockx@peopleware.be>
2011-03-08 08:40:15 +0000

    structure for release I 1.0
    git-svn-id: http://ppwcode.googlecode.com/svn/dotnet/Vernacular/Persistence/I/1.n/1.0@4847 463e38ab-4f4e-0410-907a-65d2b3888964

---
+++ trunk/I/Dao/ClientDao.cs
@@ -0,0 +1,175 @@
+﻿/*
+ * Copyright 2004 - $Date: 2008-11-15 23:58:07 +0100 (za, 15 nov 2008) $ by PeopleWare n.v..
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#region Using
+
+using System;
+using System.Diagnostics.Contracts;
+using System.Reflection;
+using System.ServiceModel;
+
+using log4net;
+
+using PPWCode.Vernacular.Persistence.I.Dao.Wcf.Helpers;
+
+#endregion
+
+namespace PPWCode.Vernacular.Persistence.I.Dao
+{
+    public abstract class ClientDao :
+        IDisposable
+    {
+        #region Fields
+
+        private static readonly ILog s_Logger = LogManager.GetLogger(typeof(ClientDao));
+
+        #endregion
+
+        #region Invariant
+
+        [ContractInvariantMethod]
+        // ReSharper disable UnusedMember.Local
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(Obj != null);
+        }
+
+        // ReSharper restore UnusedMember.Local
+
+        #endregion
+
+        #region Constructor
+
+        protected ClientDao(object obj)
+        {
+            Contract.Requires(obj != null);
+            Contract.Ensures(obj == Obj);
+
+            m_Obj = obj;
+        }
+
+        #endregion
+
+        #region Properties
+
+        private object m_Obj;
+
+        [Pure]
+        // ReSharper disable MemberCanBePrivate.Global
+            public object Obj
+        {
+            get { return m_Obj; }
+        }
+
+        // ReSharper restore MemberCanBePrivate.Global
+
+        #endregion
+
+        #region Protected Methods
+
+        private void CloseProxy(ICommunicationObject co)
+        {
+            if (!m_Disposed)
+            {
+                CommunicationProxyHelper.Close(co);
+            }
+        }
+
+        #endregion
+
+        #region Implementation of IDisposable
+
+        ~ClientDao()
+        {
+            lock (m_Locker)
+            {
+                if (!m_Disposed)
+                {
+                    s_Logger.Warn("Code smell: Call dispose directly instead of relying on the finalizer.");
+                    SafeCleanup();
+                }
+            }
+        }
+
+        private readonly object m_Locker = new object();
+        private bool m_Disposed;
+
+        protected bool Disposed
+        {
+            get
+            {
+                lock (m_Locker)
+                {
+                    return m_Disposed;
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            lock (m_Locker)
+            {
+                if (!m_Disposed)
+                {
+                    SafeCleanup();
+                    m_Disposed = true;
+                    GC.SuppressFinalize(this);
+                }
+            }
+        }
+
+        private void SafeCleanup()
+        {
+            try
+            {
+                Cleanup();
+            }
+            catch (Exception e)
+            {
+                s_Logger.Warn(MethodBase.GetCurrentMethod().Name, e);
+            }
+        }
+
+        protected virtual void Cleanup()
+        {
+            if (m_Obj == null)
+            {
+                return;
+            }
+            ICommunicationObject co = m_Obj as ICommunicationObject;
+            if (co != null)
+            {
+                CloseProxy(co);
+            }
+            IDisposable disposableObj = m_Obj as IDisposable;
+            if (disposableObj != null)
+            {
+                disposableObj.Dispose();
+            }
+            m_Obj = null;
+        }
+
+        protected void CheckObjectAlreadyDisposed()
+        {
+            if (Disposed)
+            {
+                throw new ObjectDisposedException(GetType().FullName);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ trunk/I/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PPWCode.Vernacular.Persistence.I")]
+[assembly: AssemblyDescription("Main code of the PPWCode Persistence Vernacular")]
+[assembly: AssemblyConfiguration("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6d8927ec-a8e6-4db7-a8e0-9ef5500b257f")]
