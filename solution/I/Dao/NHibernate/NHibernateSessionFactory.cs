2f7fae9c90f34fd2d0e7b57144ad4e2b4f3c001e
Jan Dockx <jan_dockx@peopleware.be>
2011-03-21 18:19:16 +0000

    release I 1.1
    git-svn-id: http://ppwcode.googlecode.com/svn/dotnet/Vernacular/Persistence/I/1.n/1.1@4925 463e38ab-4f4e-0410-907a-65d2b3888964

---
+++ solution/I/Dao/NHibernate/NHibernateSessionFactory.cs
@@ -0,0 +1,118 @@
+﻿/*
+ * Copyright 2004 - $Date: 2008-11-15 23:58:07 +0100 (za, 15 nov 2008) $ by PeopleWare n.v..
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#region Using
+
+using System.Collections.Generic;
+using System.Reflection;
+
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Event;
+
+#endregion
+
+namespace PPWCode.Vernacular.Persistence.I.Dao.NHibernate
+{
+    public static class NHibernateSessionFactory
+    {
+        #region fields
+
+        private static readonly object s_Lock = new object();
+        private static readonly Dictionary<string, ISessionFactory> s_SessionFactories = new Dictionary<string, ISessionFactory>();
+
+        #endregion
+
+        #region Private helpers
+
+        private static Configuration CreateConfiguration(HashSet<Assembly> assemblies)
+        {
+            Configuration cfg = new Configuration();
+
+            cfg.EventListeners.PreUpdateEventListeners = new IPreUpdateEventListener[]
+            {
+                new AuditEventListener(),
+            };
+            cfg.EventListeners.PreInsertEventListeners = new IPreInsertEventListener[]
+            {
+                new AuditEventListener(),
+            };
+            cfg.EventListeners.PostUpdateEventListeners = new IPostUpdateEventListener[]
+            {
+                new AuditLogEventListner(),
+            };
+            cfg.EventListeners.PostInsertEventListeners = new IPostInsertEventListener[]
+            {
+                new AuditLogEventListner(),
+            };
+            cfg.EventListeners.PostDeleteEventListeners = new IPostDeleteEventListener[]
+            {
+                new AuditLogEventListner(),
+            };
+
+            if (assemblies == null)
+            {
+                assemblies = new HashSet<Assembly>();
+            }
+            assemblies.Add(typeof(AuditLog).Assembly);
+            foreach (Assembly assembly in assemblies)
+            {
+                cfg.AddAssembly(assembly);
+            }
+
+            return cfg.Configure();
+        }
+
+        private static ISessionFactory CreateSessionFactory(IEnumerable<KeyValuePair<string, string>> properties, HashSet<Assembly> assemblies)
+        {
+            Configuration cfg = CreateConfiguration(assemblies);
+            foreach (var item in properties)
+            {
+                cfg.Properties.Add(item);
+            }
+            return cfg.BuildSessionFactory();
+        }
+
+        #endregion
+
+        public static ISessionFactory CreateSessionFactory(string connectionStringName, IDictionary<string, string> properties, HashSet<Assembly> assemblies)
+        {
+            lock (s_Lock)
+            {
+                ISessionFactory result;
+                if (!s_SessionFactories.TryGetValue(connectionStringName, out result))
+                {
+                    if (properties == null)
+                    {
+                        properties = new Dictionary<string, string>();
+                    }
+                    properties.Add("connection.connection_string_name", connectionStringName);
+                    result = CreateSessionFactory(properties, assemblies);
+                    s_SessionFactories.Add(connectionStringName, result);
+                }
+                return result;
+            }
+        }
+
+        public static ISessionFactory GetSessionFactory(string connectionStringName)
+        {
+            lock (s_Lock)
+            {
+                return s_SessionFactories[connectionStringName];
+            }
+        }
+    }
+}
---
+++ solution/I/Dao/PPWSecurityPermissionAttribute.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * Copyright 2004 - $Date: 2008-11-15 23:58:07 +0100 (za, 15 nov 2008) $ by PeopleWare n.v..
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#region Using
+
+using System;
+using System.Runtime.InteropServices;
+
+#endregion
+
+namespace PPWCode.Vernacular.Persistence.I.Dao
+{
+    [Serializable, Flags, ComVisible(true)]
+    public enum SecurityActionFlag : byte
+    {
+        /// <summary>
+        /// Create
+        /// </summary>
+        CREATE = 0x01,
+        /// <summary>
+        /// Retrieve
+        /// </summary>
+        RETRIEVE = 0x02,
+        /// <summary>
+        /// Update
+        /// </summary>
+        UPDATE = 0x04,
+        /// <summary>
+        /// Delete
+        /// </summary>
+        DELETE = 0x08,
+        /// <summary>
+        /// AllPermissions
+        /// </summary>
+        ALL_PERMISSIONS = CREATE | RETRIEVE | UPDATE | DELETE,
+    }
+
+    [Serializable, AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false), ComVisible(true)]
+    public class PPWSecurityActionAttribute : Attribute
+    {
+        public SecurityActionFlag SecurityAction { get; set; }
+        public string Role { get; set; }
+    }
+}
