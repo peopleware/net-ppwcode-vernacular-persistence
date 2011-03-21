8d523d550bea8d94db9c4c7ace81a44c6cff3f27
Jan Dockx <jan_dockx@peopleware.be>
2011-03-21 18:17:13 +0000

    * fixing buggy UriUserType * now we always save AbsoluteUri, canonical; only absolute URI's allowed * also supports wild cards
    git-svn-id: http://ppwcode.googlecode.com/svn/dotnet/Vernacular/Persistence/trunk@4923 463e38ab-4f4e-0410-907a-65d2b3888964

--- I/Dao/NHibernate/UriUserType.cs
+++ I/Dao/NHibernate/UriUserType.cs
@@ -14,6 +14,8 @@
  * limitations under the License.
  */
 
+#region Using
+
 using System;
 using System.Data;
 
@@ -21,103 +23,167 @@ using NHibernate;
 using NHibernate.SqlTypes;
 using NHibernate.UserTypes;
 
+#endregion
+
 namespace PPWCode.Vernacular.Persistence.I.Dao.NHibernate
 {
+    /// <summary>
+    /// <see cref="IUserType"/> for <see cref="Uri"/> instances,
+    /// with support for wildcards in a &quot;Like&quot; criterium
+    /// expression.
+    /// </summary>
+    /// <remarks>
+    /// <para><see cref="Uri"/> instances are stored as canonical strings.</para>
+    /// <para>We only support <see cref="Uri.IsAbsoluteUri">absolute URI's</see></para>
+    /// </remarks>
     public class UriUserType : IUserType
     {
-     public SqlType[] SqlTypes
-     {
-         get
-         {
-             //We store our Uri in a single column in the database that can contain a string
-             SqlType[] types = new SqlType[1];
-             types[0] = new SqlType(DbType.String);
-             return types;
-         }
-     }
+        /// <summary>
+        /// We store our Uri in a single column in the database that can contain a string.
+        /// </summary>
+        public SqlType[] SqlTypes
+        {
+            get
+            {
+                SqlType[] types = new SqlType[1];
+                types[0] = new SqlType(DbType.String);
+                return types;
+            }
+        }
 
-      public Type ReturnedType
-      {
-          get { return typeof(Uri); }
-      }
+        /// <summary>
+        /// This is a user type for <see cref="Uri"/> instances.
+        /// </summary>
+        public Type ReturnedType
+        {
+            get
+            {
+                return typeof(Uri);
+            }
+        }
 
-      public new bool Equals(object x, object y)
-      {
-          //Uri implements Equals it self by comparing the Uri's based
-          // on value so we use this implementation
-          if (x == null)
-          {
-              return false;
-          }
-          else
-          {
-              return x.Equals(y);
-          }
-      }
+        /// <summary>
+        /// <see cref="Uri"/> implements <see cref="Uri.Equals(object)"/>
+        /// it self by comparing the Uri's based
+        /// on value so we use this implementation.
+        /// </summary>
+        public new bool Equals(object x, object y)
+        {
+            if (x == null)
+            {
+                return false;
+            }
+            return x.Equals(y);
+        }
 
-      public int GetHashCode(object x)
-      {
-          //Again URL itself implements GetHashCode so we use that
-          return x.GetHashCode();
-      }
+        /// <summary>
+        /// <see cref="Uri"/> implements <see cref="Uri.GetHashCode"/>,
+        /// so we use that.
+        /// </summary>
+        public int GetHashCode(object x)
+        {
+            return x.GetHashCode();
+        }
 
-      public object NullSafeGet(IDataReader rs, string[] names, object owner)
-      {
-          //We get the string from the database using the NullSafeGet used to get strings
-          string uriString = (string) NHibernateUtil.String.NullSafeGet(rs, names[0]);
+        /// <summary>
+        /// Turn a canonical representation of a URI, retrieved
+        /// from the database, into a <see cref="Uri"/>.
+        /// </summary>
+        /// <returns>We throw an error if what we get from
+        /// the database is not a canonical URI.</returns>
+        public object NullSafeGet(IDataReader rs, string[] names, object owner)
+        {
+            //We get the string from the database using the NullSafeGet used to get strings
+            string uriString = (string)NHibernateUtil.String.NullSafeGet(rs, names[0]);
+            if (uriString == null)
+            {
+                return null;
+            }
+            if (! Uri.IsWellFormedUriString(uriString, UriKind.Absolute))
+            {
+                throw new HibernateException("Trying to create a Uri from a string from the DB which is not a valid absolute Uri string: " + uriString);
+            }
+            //And save it in the Uri object. This would be the place to make sure that your string
+            //is valid for use with the System.Uri class, but i will leave that to you
+            Uri result = new Uri(uriString, UriKind.Absolute);
+            return result;
+        }
 
-          //And save it in the Uri object. This would be the place to make sure that your string
-          //is valid for use with the System.Uri class, but i will leave that to you
-          Uri result = new Uri(uriString);
-          return result;
-      }
+        public const string UriLikeWildcard = @"PPWCODE.ULWC";
 
-      public void NullSafeSet(IDbCommand cmd, object value, int index)
-      {
-          Uri uri = value as Uri;
-          //Set the value using the NullSafeSet implementation for string from NHibernateUtil
-          if (uri == null)
-          {
-              NHibernateUtil.String.NullSafeSet(cmd, null, index);
-              return;
-          }
-          //ToString called on Uri instance
-          value = uri.OriginalString;
-          NHibernateUtil.String.NullSafeSet(cmd, value, index);
-      }
+        /// <summary>
+        /// Write a <paramref name="value"/> that is a <see cref="Uri"/>
+        /// into the <paramref name="cmd"/> at position <paramref name="index"/>
+        /// as the canonical representation of the URI.
+        /// </summary>
+        /// <remarks>
+        /// <para>This is also used by Hibernate for parameters in a &quot;Like&quot;
+        /// query. In a &quot;Like&quot; query, we want to use a wild card.
+        /// We recognize a <see cref="Uri"/> as being used as parameter in a
+        /// &quot;Like&quot; if we find the wild card pattern in the
+        /// <see cref="Uri"/> canonical string.</para>
+        /// </remarks>
+        public void NullSafeSet(IDbCommand cmd, object value, int index)
+        {
+            if (value != null && (! (value is Uri)))
+            {
+                throw new HibernateException("Trying to use UriUserType with " + value + ", which is not a Uri");
+            }
+            string canonicalString;
+            if (value == null)
+            {
+                canonicalString = null;
+            }
+            else
+            {
+                Uri uri = (Uri)value;
+                if (! uri.IsAbsoluteUri)
+                {
+                    throw new HibernateException("Trying to use UriUserTYpe with " + uri + ", which is not an absolute URI");
+                }
+                canonicalString = uri.AbsoluteUri;
+                // canonical string is guaranteed not null
+                if (canonicalString.Contains(UriLikeWildcard))
+                {
+                    // we are being used as a like parameter
+                    canonicalString = canonicalString.Replace(@"%", @"[%]");
+                    canonicalString = canonicalString.Replace(@"_", @"[_]");
+                    canonicalString = canonicalString.Replace(@"?", @"[?]");
+                    canonicalString = canonicalString.Replace(UriLikeWildcard, @"%");
+                }
+            }
+            NHibernateUtil.String.NullSafeSet(cmd, canonicalString, index);
+        }
 
-      public object DeepCopy(object value)
-      {
-          Uri uri = value as Uri;
-          // We deep copy the uri by creating a new instance with the same contents
-          if (uri == null)
-          {
-              return null;
-          }
-          return new Uri(uri.OriginalString);
-      }
+        public object DeepCopy(object value)
+        {
+            return value;
+        }
 
-      public bool IsMutable
-      {
-          get { return false; }
-      }
+        public bool IsMutable
+        {
+            get
+            {
+                return false;
+            }
+        }
 
-      public object Replace(object original, object target, object owner)
-      {
-          //As our object is immutable we can just return the original
-          return original;
-      }
+        public object Replace(object original, object target, object owner)
+        {
+            //As our object is immutable we can just return the original
+            return original;
+        }
 
-      public object Assemble(object cached, object owner)
-      {
-          //Used for casching, as our object is immutable we can just return it as is
-          return cached;
-      }
+        public object Assemble(object cached, object owner)
+        {
+            //Used for casching, as our object is immutable we can just return it as is
+            return cached;
+        }
 
-      public object Disassemble(object value)
-      {
-          //Used for casching, as our object is immutable we can just return it as is
-          return value;
-      }
+        public object Disassemble(object value)
+        {
+            //Used for casching, as our object is immutable we can just return it as is
+            return value;
+        }
     }
 }
