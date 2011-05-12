409be9ab1617617c9111b110115be22f43dc8ab0
Danny Van den Wouwer <danny_van_den_wouwer@peopleware.be>
2011-05-12 13:28:53 +0000

    * Better usage of AssemblyVersion / AssemblyFileVersion ==> avoid exceptions in VS using the winform designer that a specific assembly isn't found
    git-svn-id: http://ppwcode.googlecode.com/svn/dotnet/Vernacular/Persistence/I/latest@5076 463e38ab-4f4e-0410-907a-65d2b3888964

---
+++ solution/Solution Items/SolutionInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+
+[assembly: AssemblyCompany("Peopleware NV")]
+[assembly: AssemblyProduct("PPWCode Vernacular Persistence")]
+[assembly: AssemblyCopyright("Copyright © PeopleWare NV 2010-2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.5072.0")]
