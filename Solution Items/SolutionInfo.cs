25f49f51b4466ee42bc1ce6c0c50792881f15a9b
Danny Van den Wouwer <danny_van_den_wouwer@peopleware.be>
2011-05-23 14:53:37 +0000

    * New version 1.3 Better support for Distributed Transactions
    git-svn-id: http://ppwcode.googlecode.com/svn/dotnet/Vernacular/Persistence/trunk@5117 463e38ab-4f4e-0410-907a-65d2b3888964

--- Solution Items/SolutionInfo.cs
+++ Solution Items/SolutionInfo.cs
@@ -5,5 +5,5 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © PeopleWare NV 2010-2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.5095.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.5116.0")]
