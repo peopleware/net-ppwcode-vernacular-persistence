c3cf48f705b8a7f2130b5b4ef234e14cdd5c4308
Danny Van den Wouwer <danny_van_den_wouwer@peopleware.be>
2011-05-29 16:25:33 +0000

    * Better support for specifying a SessionFactory when using NHibernatContextAttribute
    git-svn-id: http://ppwcode.googlecode.com/svn/dotnet/Vernacular/Persistence/I/latest@5161 463e38ab-4f4e-0410-907a-65d2b3888964

---
+++ solution/I/Dao/NHibernate/NHibernateSerializationContractBehaviorAttribute.cs
@@ -0,0 +1,57 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+
+#endregion
+
+namespace PPWCode.Vernacular.Persistence.I.Dao.NHibernate
+{
+    [AttributeUsage(AttributeTargets.Interface)]
+    public class NHibernateSerializationContractBehaviorAttribute :
+        Attribute,
+        IContractBehavior
+    {
+        #region Private Helpers
+
+        private static void ApplyDataContractSurrogate(ContractDescription contractDescription)
+        {
+            IEnumerable<DataContractSerializerOperationBehavior> dataContractSerializerOperationBehaviors = contractDescription
+                .Operations
+                .Select(operation => operation.Behaviors.Find<DataContractSerializerOperationBehavior>())
+                .Where(dataContractBehavior => dataContractBehavior != null);
+            foreach (DataContractSerializerOperationBehavior dataContractBehavior in dataContractSerializerOperationBehaviors)
+            {
+                dataContractBehavior.DataContractSurrogate = new NHibernateDataContractSurrogate();
+            }
+        }
+
+        #endregion
+
+        #region Implementation of IContractBehavior
+
+        void IContractBehavior.Validate(ContractDescription contractDescription, ServiceEndpoint endpoint)
+        {
+        }
+
+        void IContractBehavior.ApplyDispatchBehavior(ContractDescription contractDescription, ServiceEndpoint endpoint, DispatchRuntime dispatchRuntime)
+        {
+            ApplyDataContractSurrogate(contractDescription);
+        }
+
+        void IContractBehavior.ApplyClientBehavior(ContractDescription contractDescription, ServiceEndpoint endpoint, ClientRuntime clientRuntime)
+        {
+            ApplyDataContractSurrogate(contractDescription);
+        }
+
+        void IContractBehavior.AddBindingParameters(ContractDescription contractDescription, ServiceEndpoint endpoint, BindingParameterCollection bindingParameters)
+        {
+        }
+
+        #endregion
+    }
+}
