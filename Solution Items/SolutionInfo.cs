55523de3657758d2a9d5d3088685797318579162
Danny Van den Wouwer <danny_van_den_wouwer@peopleware.be>
2011-05-30 19:07:34 +0000

    * Lets make client AND server endpoint configurable when using InprocHosting
    git-svn-id: http://ppwcode.googlecode.com/svn/dotnet/Vernacular/Persistence/trunk@5172 463e38ab-4f4e-0410-907a-65d2b3888964

--- Solution Items/SolutionInfo.cs
+++ Solution Items/SolutionInfo.cs
@@ -6,4 +6,4 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.5141.0")]
+[assembly: AssemblyFileVersion("1.3.5158.0")]
