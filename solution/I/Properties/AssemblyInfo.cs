03d6b7aa4860901e541e9f9e49c94df30447154e
Danny Van den Wouwer <danny_van_den_wouwer@peopleware.be>
2011-08-09 18:41:56 +0000

    * Allow finer graining for instantiating SessionFactories * Don't purge MSMQ after closing a host with MSMQ bindings
    git-svn-id: http://ppwcode.googlecode.com/svn/dotnet/Vernacular/Persistence/I/latest@5232 463e38ab-4f4e-0410-907a-65d2b3888964

---
+++ solution/I/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿#region Using
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#endregion
+
+[assembly: AssemblyTitle("PPWCode.Vernacular.Persistence.I")]
+[assembly: AssemblyDescription("Main code of the PPWCode Persistence Vernacular")]
+[assembly: AssemblyConfiguration("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("6d8927ec-a8e6-4db7-a8e0-9ef5500b257f")]
