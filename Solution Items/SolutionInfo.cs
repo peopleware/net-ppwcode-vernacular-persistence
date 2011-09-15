229b7475c226a0357388b5c6867e90fd74d7c0b2
Danny Van den Wouwer <danny_van_den_wouwer@peopleware.be>
2011-09-15 06:29:24 +0000

    * adjust AssemblyFileVersion
    git-svn-id: http://ppwcode.googlecode.com/svn/dotnet/Vernacular/Persistence/trunk@5320 463e38ab-4f4e-0410-907a-65d2b3888964

--- Solution Items/SolutionInfo.cs
+++ Solution Items/SolutionInfo.cs
@@ -6,4 +6,4 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.5303.0")]
+[assembly: AssemblyFileVersion("1.3.5319.0")]
