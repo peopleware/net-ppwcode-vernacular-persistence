e5129bad4a63e50b69a72eaffa62e28ee7a1ff32
Ruben Vandeginste <ruben_vandeginste@peopleware.be>
2012-02-20 15:56:54 +0000

    SEMPERA-1518 - upping dependency on OddsAndEnds to 2.0
    git-svn-id: http://ppwcode.googlecode.com/svn/dotnet/Vernacular/Persistence/trunk@6069 463e38ab-4f4e-0410-907a-65d2b3888964

--- src/Solution Items/SolutionInfo.cs
+++ src/Solution Items/SolutionInfo.cs
@@ -5,5 +5,9 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © PeopleWare NV 2010-2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
+// assembly version: up only when not backwards compatible
 [assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.5319.0")]
+
+// assembly file version, keep in sync with package version
+[assembly: AssemblyFileVersion("1.3.1.0")]
