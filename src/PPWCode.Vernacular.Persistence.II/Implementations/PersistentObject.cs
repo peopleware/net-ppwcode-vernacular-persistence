e1c25d232b4ce35d9036ba9a6ece92b2f639bbde
Ruben Vandeginste <ruben.vandeginste@gmail.com>
2014-07-06 19:25:25 +0200

    Reformatting code, fixing license headers. Fixing namespace for exceptions.

--- src/PPWCode.Vernacular.Persistence.II/Implementations/PersistentObject.cs
+++ src/PPWCode.Vernacular.Persistence.II/Implementations/PersistentObject.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿// Copyright 2014 by PeopleWare n.v..
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
 using System.Linq;
@@ -30,9 +44,9 @@ namespace PPWCode.Vernacular.Persistence.II
         }
 
         /// <summary>
-        /// A call to <see cref="ICivilizedObject.WildExceptions"/>
-        /// returns an <see cref="CompoundSemanticException.IsEmpty"/>
-        /// exception.
+        ///     A call to <see cref="ICivilizedObject.WildExceptions" />
+        ///     returns an <see cref="CompoundSemanticException.IsEmpty" />
+        ///     exception.
         /// </summary>
         [Pure]
         public virtual bool IsCivilized
@@ -41,18 +55,22 @@ namespace PPWCode.Vernacular.Persistence.II
         }
 
         /// <summary>
-        /// Build a set of <see cref="CompoundSemanticException"/> instances
-        /// that tell what is wrong with this instance, with respect to
-        /// <em>being civilized</em>.
+        ///     Build a set of <see cref="CompoundSemanticException" /> instances
+        ///     that tell what is wrong with this instance, with respect to
+        ///     <em>being civilized</em>.
         /// </summary>
         /// <returns>
-        /// <para>The result comes in the form of an <strong>unclosed</strong>
-        /// <see cref="CompoundSemanticException"/>, of
-        /// which the set of element exceptions might be empty.</para>
-        /// <para>This method should work in any state of the object.</para>
-        /// <para>This method is public instead of
-        /// protected to make it more easy to describe to users what the business
-        /// rules for this type are.</para>
+        ///     <para>
+        ///         The result comes in the form of an <strong>unclosed</strong>
+        ///         <see cref="CompoundSemanticException" />, of
+        ///         which the set of element exceptions might be empty.
+        ///     </para>
+        ///     <para>This method should work in any state of the object.</para>
+        ///     <para>
+        ///         This method is public instead of
+        ///         protected to make it more easy to describe to users what the business
+        ///         rules for this type are.
+        ///     </para>
         /// </returns>
         [Pure]
         public virtual CompoundSemanticException WildExceptions()
@@ -61,16 +79,20 @@ namespace PPWCode.Vernacular.Persistence.II
         }
 
         /// <summary>
-        /// Call <see cref="ICivilizedObject.WildExceptions"/>, and if the result
-        /// is not <see cref="CompoundSemanticException.IsEmpty"/>,
-        /// close the exception and throw it.
+        ///     Call <see cref="ICivilizedObject.WildExceptions" />, and if the result
+        ///     is not <see cref="CompoundSemanticException.IsEmpty" />,
+        ///     close the exception and throw it.
         /// </summary>
         /// <remarks>
-        /// <para>This method has no effects. If it ends nominally,
-        /// and if it throws an exception, no state is changed.</para>
-        /// <para>It is not <c>[Pure]</c> however, since it changes
-        /// the state of the exception to
-        /// <see cref="CompoundSemanticException.Closed"/>.</para>
+        ///     <para>
+        ///         This method has no effects. If it ends nominally,
+        ///         and if it throws an exception, no state is changed.
+        ///     </para>
+        ///     <para>
+        ///         It is not <c>[Pure]</c> however, since it changes
+        ///         the state of the exception to
+        ///         <see cref="CompoundSemanticException.Closed" />.
+        ///     </para>
         /// </remarks>
         public virtual void ThrowIfNotCivilized()
         {
@@ -102,14 +124,17 @@ namespace PPWCode.Vernacular.Persistence.II
             {
                 return false;
             }
+
             if (ReferenceEquals(this, other))
             {
                 return true;
             }
+
             if (IsTransient || other.IsTransient)
             {
                 return false;
             }
+
             if (!EqualityComparer<T>.Default.Equals(Id, other.Id))
             {
                 return false;
@@ -121,10 +146,10 @@ namespace PPWCode.Vernacular.Persistence.II
         }
 
         /// <summary>
-        /// Returns a string that represents the current object.
+        ///     Returns a string that represents the current object.
         /// </summary>
         /// <returns>
-        /// A string that represents the current object.
+        ///     A string that represents the current object.
         /// </returns>
         public override string ToString()
         {
@@ -145,8 +170,10 @@ namespace PPWCode.Vernacular.Persistence.II
                 {
                     value = e.GetBaseException().Message;
                 }
+
                 sb.AppendFormat("; {0} = '{1}'", propertyInfo.Name, value);
             }
+
             return sb.ToString();
         }
     }
