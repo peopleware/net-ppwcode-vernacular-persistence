c4f77dd208226171c5b5a538dbca680ec98941a4
Ruben Vandeginste <ruben_vandeginste@peopleware.be>
2015-07-12 13:51:02 +0200

    Bumping version of PPWCode dependencies.

--- src/I/Properties/AssemblyInfo.cs
+++ src/I/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 #endregion
 
 [assembly: AssemblyTitle("PPWCode.Vernacular.Persistence.I")]
-[assembly: AssemblyDescription("Main code of the PPWCode Persistence Vernacular")]
+[assembly: AssemblyDescription("PPWCode.Vernacular.Persistence.I")]
 [assembly: AssemblyConfiguration("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("6d8927ec-a8e6-4db7-a8e0-9ef5500b257f")]
