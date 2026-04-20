081d628be1a0a773190d700d739010f509e47496
Danny Van den Wouwer <dvdwouwe@peopleware.be>
2026-04-20 12:01:38 +0200

    PPWCODE-15 Add support for versioned objects

--- src/V/UpdateAuditableVersionedPersistentObject.cs
+++ src/V/UpdateAuditableVersionedPersistentObject.cs
@@ -9,12 +9,19 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-namespace PPWCode.Vernacular.Persistence.V;
-
-public interface IInsertAuditable<TTimestamp>
-    where TTimestamp : struct, IComparable<TTimestamp>, IEquatable<TTimestamp>
+namespace PPWCode.Vernacular.Persistence.V
 {
-    TTimestamp? CreatedAt { get; set; }
+    public abstract class UpdateAuditableVersionedPersistentObject<T, TVersion, TTimestamp>
+        : VersionedPersistentObject<T, TVersion>,
+          IUpdateAuditable<TTimestamp>
+        where T : IEquatable<T>
+        where TVersion : IEquatable<TVersion>
+        where TTimestamp : struct, IComparable<TTimestamp>, IEquatable<TTimestamp>
+    {
+        [AuditLogPropertyIgnore]
+        public virtual TTimestamp? LastModifiedAt { get; set; }
 
-    string? CreatedBy { get; set; }
+        [AuditLogPropertyIgnore]
+        public virtual string? LastModifiedBy { get; set; }
+    }
 }
---
+++ src/V/VersionedPersistentObject.cs
@@ -0,0 +1,48 @@
+﻿// Copyright 2026 by PeopleWare n.v..
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace PPWCode.Vernacular.Persistence.V
+{
+    public abstract class VersionedPersistentObject<T, TVersion>
+        : PersistentObject<T>,
+          IVersionedPersistentObject<T, TVersion>
+        where T : IEquatable<T>
+        where TVersion : IEquatable<TVersion>
+    {
+        [AuditLogPropertyIgnore]
+        public virtual TVersion? PersistenceVersion { get; init; }
+
+        public override bool IsSame(IIdentity<T>? other)
+        {
+            if (ReferenceEquals(null, other))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            if (!(other is IVersionedPersistentObject<T, TVersion> versionedOther))
+            {
+                return base.IsSame(other);
+            }
+
+            EqualityComparer<TVersion> equalityComparer = EqualityComparer<TVersion>.Default;
+
+            return base.IsSame(other)
+                   && !equalityComparer.Equals(PersistenceVersion, default(TVersion))
+                   && !equalityComparer.Equals(versionedOther.PersistenceVersion, default(TVersion))
+                   && equalityComparer.Equals(PersistenceVersion, versionedOther.PersistenceVersion);
+        }
+    }
+}
